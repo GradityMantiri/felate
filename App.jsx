ecce5352a6ae8aad8b4b4e5ccc973f0fa6ce8cd7
Gradity Mantiri <mantirigradity29@gmail.com>
2023-11-24 11:30:00 +0800

    first commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Header from "./Main/Header";
+import Home from "./Main/Home";
+import Footer from "./Main/Footer";
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
