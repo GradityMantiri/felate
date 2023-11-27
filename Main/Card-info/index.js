9b1ee3e4021b6391d003ee1878446ef60b9f5d83
Firgin <120155575+firginiaflo@users.noreply.github.com>
2023-11-27 12:53:07 +0800

    Add files via upload

---
+++ Main/Card-info/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Evolution from "../Evolution";
+import TypeEffect from "../Type-effect";
+import Logo from "../Logo";
+
+const CardInfo = () => {
+  return (
+    <div>
+      <h2>cardInfo</h2>
+      <Evolution />
+      <TypeEffect />
+      <Logo />
+    </div>
+  );
+};
+
+export default CardInfo;
