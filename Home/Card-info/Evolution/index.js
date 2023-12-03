850bcb8b3a8d6501cf19526f6a8dbccc960a970d
Firgin <120155575+firginiaflo@users.noreply.github.com>
2023-12-03 23:32:26 +0800

    index.js

--- Home/Card-info/Evolution/index.js
+++ Home/Card-info/Evolution/index.js
@@ -1,11 +1,7 @@
 import React from 'react'
 
 const Evolution = () => {
-  return (
-    <div>
-      <p>evolution</p>
-    </div>
-  )
-}
+  return <button>{title}</button>;
+};
 
 export default Evolution
