0ee98deeee79131afe0b5a433e856979fbc0f478
AndrewRett <146518775+AndrewRett@users.noreply.github.com>
2023-12-03 23:15:52 +0800

    props footer

--- Home/Footer/index.jsx
+++ Home/Footer/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const Footer = () => {
+const Footer = (props) => {
   return (
     <div>
         <h1>footer</h1>
+        <h2>{props.title}</h2>
+        {/* Contoh penggunaan props lainnya */}
+        <p>{props.description}</p>
     </div>
   )
 }
